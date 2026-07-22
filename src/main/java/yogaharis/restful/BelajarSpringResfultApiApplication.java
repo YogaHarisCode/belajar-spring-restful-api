f6a5b161ab00693aaed10e1d58134b2fa3684c8b
Yoga Harisistianugroho <yogahariss@gmail.com>
2026-07-22 16:32:50 +0700

    initialize project

---
+++ src/main/java/yogaharis/restful/BelajarSpringResfultApiApplication.java
@@ -0,0 +1,13 @@
+package yogaharis.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringResfultApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringResfultApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yogaharis/restful/BelajarSpringResfultApiApplicationTests.java
@@ -0,0 +1,13 @@
+package yogaharis.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringResfultApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
